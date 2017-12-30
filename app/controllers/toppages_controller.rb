4149f0ff900515e411b0b1a04de82530eb7b1a11
kpomk <b.77jkla@gmail.com>
2017-12-30 00:19:32 +0000

    toppageranking

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,12 +3,21 @@ class ToppagesController < ApplicationController
   
   def index
     article_id = Like.group(:article_id).order("count_all desc").count.first.first
-    @articles=Article.find(article_id)
+    #@articles=Article.find(article_id)
     #@articles= Article.all.page(params[:page]).per(6)
+    
+    #@articles=Article.all.page(params[:page]).per(6)
+    
+    @newarrivals = Article.all.order('created_at DESC').page(params[:page]).per(4)
+    
+    @ranking_counts = Like.ranking
+    @ranking_articles = Article.where(id: @ranking_counts.keys).page(params[:page]).per(4)
+    
     if params[:category_id]
       @items = Article.where(category_id: params[:category_id])
     else
       @items = Article.all
     end
   end
+
 end
