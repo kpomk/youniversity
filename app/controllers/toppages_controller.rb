6a167353912d09282b6173bb1f12832d1381edeb
kpomk <b.77jkla@gmail.com>
2018-01-04 12:45:15 +0000

    cuont

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   skip_before_action :require_user_logged_in
   
   def index
-    article_id = Like.group(:article_id).order("count_all desc").count.first.first
+    article_id = Like.group(:article_id).order("count_all desc").count
     #@articles=Article.find(article_id)
     #@articles= Article.all.page(params[:page]).per(6)
     
