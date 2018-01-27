03a8477a5386fd3a4f7e40a6e5474181c9806809
kpomk <b.77jkla@gmail.com>
2018-01-27 10:48:58 +0000

    退会ボタン移動

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
  get 'login', to: 'sessions#new'
  post 'login', to: 'sessions#create'
  delete 'logout', to: 'sessions#destroy'
+ delete 'exit', to: 'users#destroy'
  
  get 'signup', to: 'users#new'
  resources :users, only: [:show, :new, :create, :destroy,:edit,:update] do
